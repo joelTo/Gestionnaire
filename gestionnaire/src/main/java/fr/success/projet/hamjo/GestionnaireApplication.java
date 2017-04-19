23406091d4509138bcb1d7244d968537df12fb2b
joelTo <joel.torib@gmail.com>
2017-04-19 07:38:30 +0200

    [vers.1][model]:Debut de amorcage

---
+++ gestionnaire/src/main/java/fr/success/projet/hamjo/GestionnaireApplication.java
@@ -0,0 +1,12 @@
+package fr.success.projet.hamjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionnaireApplication.class, args);
+	}
+}
